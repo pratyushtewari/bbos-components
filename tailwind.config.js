86ddba5b741ff4d7da2aa467453ba4dca1509329
Pratyush Tewari <tewari.pratyush@gmail.com>
2023-09-17 16:25:47 -0400

    added top nav logo and menu selected state

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js}"],
+  content: ["./index.html", "./src/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-brand": "#0055A5",
+      },
+      boxShadow: {
+        "selected-horizontal": "inset 0 -2px 0 #0055A5",
+      },
+    },
   },
   plugins: [],
 };
+
