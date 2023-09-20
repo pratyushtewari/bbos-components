6dacf12643a4adb37e7fda15ac8efb169f732045
Pratyush Tewari <tewari.pratyush@gmail.com>
2023-09-19 23:38:08 -0400

    added menu toggle by clicking outside event listner

--- src/navbarEvents.js
+++ src/navbarEvents.js
@@ -1,3 +1,24 @@
-const toggelAdvancedSearchMenu = (event) => {
-  event.target.nextElementSibling.classList.toggle("hidden");
+const toggleTopNavMenu = (event) => {
+  event.stopPropagation();
+  const sibling = event.target.nextElementSibling;
+  sibling.classList.toggle("hidden");
+
+  console.log(sibling.id);
+  console.log(sibling.classList);
+
+  if (!sibling.classList.contains("hidden")) {
+    window.addEventListener(
+      "click",
+      (event) => {
+        console.log(sibling.id);
+        sibling.classList.add("hidden");
+      },
+      { once: true },
+    );
+  }
 };
+
+function hide(htmlElement) {
+  console.log(htmlElement.id);
+  htmlElement.classList.add("hidden");
+}
