6f5fab749af9f094c00da42b45be4daea117051d
Pratyush Tewari <tewari.pratyush@gmail.com>
2023-09-20 23:33:54 -0400

    added escape key

--- src/navbarEvents.js
+++ src/navbarEvents.js
@@ -1,15 +1,21 @@
-function hide(htmlElement) {
-  console.log("closing", htmlElement.id);
+const hide = (htmlElement) => {
   htmlElement.classList.add("hidden");
 }
 
-function closeALLMenu() {
+const closeALLMenu = () => {
   [...document.querySelectorAll(".menu:not(.hidden)")].forEach(
     (htmlElement) => {
       htmlElement.classList.add("hidden");
     },
   );
 }
+
+// Escape key press
+document.addEventListener('keydown', function(e) {
+  if(e.keyCode == 27){
+    closeALLMenu();
+  }
+});
 
 const toggleTopNavMenu = (event) => {
   // close all the other open menus
@@ -20,8 +26,7 @@ const toggleTopNavMenu = (event) => {
   // this is just to prevent adding a lot of event listners
   // this still adds at max two listners.
   if (!sibling.classList.contains("hidden")) {
-    console.log("adding close listner for ", sibling.id);
-    window.addEventListener(
+    document.addEventListener(
       "click",
       (event) => {
         hide(sibling);
