231199a67956f4186532da58fd4d63e6ef25e706
Pratyush Tewari <tewari.pratyush@gmail.com>
2023-09-20 21:23:07 -0400

    fixed closing menus

--- src/navbarEvents.js
+++ src/navbarEvents.js
@@ -1,24 +1,30 @@
+function hide(htmlElement) {
+  console.log("closing", htmlElement.id);
+  htmlElement.classList.add("hidden");
+}
+
+function closeALLMenu() {
+  [...document.querySelectorAll(".menu")].forEach((htmlElement) => {
+    htmlElement.classList.add("hidden");
+  });
+}
+
 const toggleTopNavMenu = (event) => {
+  // close all the other open menus
+  closeALLMenu();
   event.stopPropagation();
-  const sibling = event.target.nextElementSibling;
+  const sibling = event.currentTarget.parentElement.querySelector(".menu");
   sibling.classList.toggle("hidden");
-
-  console.log(sibling.id);
-  console.log(sibling.classList);
-
+  // this is just to prevent adding a lot of event listners
+  // this still adds at max two listners.
   if (!sibling.classList.contains("hidden")) {
+    console.log("adding close listner for ", sibling.id);
     window.addEventListener(
       "click",
       (event) => {
-        console.log(sibling.id);
-        sibling.classList.add("hidden");
+        hide(sibling);
       },
       { once: true },
     );
   }
 };
-
-function hide(htmlElement) {
-  console.log(htmlElement.id);
-  htmlElement.classList.add("hidden");
-}
