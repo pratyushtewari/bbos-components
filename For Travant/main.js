afea62501e4adaacc4962f18a497a3669b4ef3a2
Pratyush Tewari <tewari.pratyush@gmail.com>
2023-09-29 18:53:00 -0400

    updated For Travant folder build

--- For Travant/main.js
+++ For Travant/main.js
@@ -1,31 +1,26 @@
 const hide = (htmlElement) => {
-  htmlElement.classList.add("hidden");
+  htmlElement.classList.add("tw-hidden");
 };
 
 const closeALLMenu = () => {
-  [...document.querySelectorAll(".bbsMenu")].forEach((htmlElement) => {
-    htmlElement.classList.add("hidden");
-  });
+  [...document.querySelectorAll(".bbsMenu:not(.tw-hidden)")].forEach(
+    (htmlElement) => {
+      htmlElement.classList.add("tw-hidden");
+    },
+  );
 };
-
-// Escape key press
-document.addEventListener("keydown", function (e) {
-  if (e.keyCode == 27) {
-    closeALLMenu();
-  }
-});
 
 const toggleTopNavMenu = (event) => {
   // close all the other open menus
   event.stopPropagation();
   const sibling = event.currentTarget.parentElement.querySelector(".bbsMenu");
   //check if this is closed. this is important to know because we are closing all the menus before opening this one.
-  const isClosed = sibling.classList.contains("hidden");
+  const isClosed = sibling.classList.contains("tw-hidden");
   closeALLMenu();
   // this is just to prevent adding a lot of event listners
   // this still adds at max two listners.
   if (isClosed) {
-    sibling.classList.remove("hidden");
+    sibling.classList.remove("tw-hidden");
     document.addEventListener(
       "click",
       (event) => {
@@ -34,6 +29,13 @@ const toggleTopNavMenu = (event) => {
       { once: true },
     );
   } else {
-    sibling.classList.add("hidden");
+    sibling.classList.add("tw-hidden");
   }
 };
+
+// Escape key press
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == 27) {
+    closeALLMenu();
+  }
+});
