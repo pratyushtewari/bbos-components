55bcd6392aaedf451dee52ada82ac1a18de4064e
Pratyush Tewari <tewari.pratyush@gmail.com>
2023-10-17 11:54:02 -0400

    added some build confilg to support travant static build

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,32 @@
 /** @type {import('vite').UserConfig} */
 import StringReplace from "vite-plugin-string-replace";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default {
+  build: {
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == "index.css") return "styles.css";
+          return assetInfo.name;
+        },
+      },
+    },
+  },
   plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: "main.js",
+          dest: "./",
+        },
+      ],
+    }),
     StringReplace([
       {
         fileName: "index.html",
-        search: /href="\/assets\//g, // search this string in content
-        replace: 'href="en-us/images/assets/', // replace search string with this
-      },
-      {
-        fileName: "index.html",
         search: /src="\/assets\//g, // you can use RegEXP to search in content
-        replace: 'src="en-us/images/assets/', // replace search string with this
+        replace: 'src="assets/', // replace search string with this
       },
     ]),
   ],
