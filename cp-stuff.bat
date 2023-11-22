a6d18d1eedd4d0203bee68fd6180efe8fd998f64
Pratyush Tewari <tewari.pratyush@gmail.com>
2023-11-21 21:57:27 -0500

    added cp commands

---
+++ cp-stuff.bat
@@ -0,0 +1,4 @@
+cp C:\Users\ptewari\work\bbos-components\dist\styles.css C:\Users\ptewari\work\vBBSi\EBB\BBOS\Content\
+cp C:\Users\ptewari\work\bbos-components\dist\main.js C:\Users\ptewari\work\vBBSi\EBB\BBOS\Scripts\
+cp C:\Users\ptewari\work\bbos-components\dist\index.html C:\Users\ptewari\work\vBBSi\EBB\BBOS\
+cp C:\Users\ptewari\work\bbos-components\search.html C:\Users\ptewari\work\vBBSi\EBB\BBOS\
